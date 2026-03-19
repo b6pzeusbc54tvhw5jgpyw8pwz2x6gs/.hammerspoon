45ddb89f373deb6a074156858a7eb7ba741074c4
Alfred <alfred@daangn.com>
2026-03-19 21:44:27 +0900

    init

---
+++ init.lua
@@ -0,0 +1,4 @@
+require('modules.inputsource_aurora')
+require('modules.switch_inputsource_to_ABC_when_enter_vim_normal_mode')
+require('modules.window_count_alert')
+
---
+++ modules/window_count_alert.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+local rules = {
+    { app = "Google Chrome", limit = 7 },
+    { app = "iTerm2",        limit = 6 },
+}
+
+local alerted = {}
+
+local function checkWindows()
+    for _, rule in ipairs(rules) do
+        local app = hs.application.get(rule.app)
+        local count = app and #app:allWindows() or 0
+        if count >= rule.limit and not alerted[rule.app] then
+            hs.alert.show("⚠ " .. rule.app .. " 창이 " .. count .. "개입니다. 정리하세요!", 3)
+            alerted[rule.app] = true
+        elseif count < rule.limit then
+            alerted[rule.app] = false
+        end
+    end
+end
+
+M.watcher = hs.window.filter.default:subscribe({
+    hs.window.filter.windowCreated,
+    hs.window.filter.windowDestroyed,
+}, checkWindows)
+
+return M
